9cf239edde8b7a55ca81c569ea7ff71a90ca2d5e
Anelle Mensah <aam2ds@virginia.edu>
2023-12-05 22:16:10 -0500

    add starter files and folders for project

---
+++ api/app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-IMPORTS-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+const express = require('express');
+const morgan = require('morgan');
+const routes = require("./routes");
+const sequelize = require("./models").sequelize;
+const cors = require('cors');
+
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-APP SET UP-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+// Creates variable to enable global error logging.
+const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+
+// Creates the Express app.
+const app = express();
+
+// Enables all CORS requests.
+app.use(cors());
+
+// Sets up request body JSON parsing.
+app.use(express.json());
+
+// Sets up morgan, which gives us http request logging.
+app.use(morgan('dev'));
+
+// Sets up a friendly greeting for the root route.
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the REST API project!',
+  });
+});
+
+// Adds routes.
+app.use('/api', routes);
+
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-ERROR HANDLERS-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route Not Found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  if (enableGlobalErrorLogging) {
+    console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+  }
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: {},
+  });
+});
+
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-DATABASE CONNECTION-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+// Sets our port.
+app.set('port', process.env.PORT || 5000);
+
+// Tests connection to the database.
+(async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log('Connection to the database successful!');
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
+})();
+
+// Starts listening on our port.
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express server is listening on port ${server.address().port}`);
+});
---
+++ api/routes.js
@@ -0,0 +1,113 @@
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-IMPORTS-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+const express = require('express');
+const router = express.Router();
+const Course = require("./models").Course;
+const User = require("./models").User;
+const {authenticateUser} = require("./middleware/auth-user");
+
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-ASYNC ERROR HANDLER-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+function asyncHandler(cb) {
+    return async (req, res, next) => {
+      try {
+        await cb(req, res, next);
+      } catch (error) {
+        // Forward error to the global error handler
+        next(error);
+      }
+    }
+  }
+
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-USER ROUTES-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+// Route that returns the currently authenticated user.
+router.get("/users", authenticateUser, asyncHandler(async (req, res)=> {
+  const user = req.currentUser;
+  res.json(user);
+}));
+
+// Route that creates a user.
+router.post("/users", asyncHandler(async (req, res) => {
+  try {
+    await User.create(req.body);
+    res.status(201).location('/').end();
+  } catch (error) {
+    console.log('ERROR: ', error.name);
+    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+      const errors = error.errors.map(err => err.message);
+      res.status(400).json({ errors });   
+    } else {
+      throw error;
+    }
+  }
+}));
+
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-COURSE ROUTES-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+// Route that returns all courses and the users associated with the course.
+router.get("/courses", asyncHandler(async (req, res)=> {
+  let courses = await Course.findAll({
+    include: [{model: User}],
+  });
+  res.json(courses);
+}));
+
+// Route that returns a specific course and the users associated with the course.
+router.get("/courses/:id", asyncHandler(async (req, res)=> {
+  const course = await Course.findByPk(req.params.id, {
+    include: [{model: User}],
+  });
+  if (course) {
+    res.json(course);
+  }
+}));
+
+// Route that creates a new course and adds it to the list of courses.
+router.post("/courses", authenticateUser, asyncHandler(async (req, res) => {
+  try {
+    const course = await Course.create(req.body);
+    res.status(201).location(`/courses/${course.id}`).end();
+  } catch (error) {
+    console.log('ERROR: ', error.name);
+    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+      const errors = error.errors.map(err => err.message);
+      res.status(400).json({ errors });   
+    } else {
+      throw error;
+    }
+  }
+}));
+
+// Route that updates an existing course.
+router.put("/courses/:id", authenticateUser, asyncHandler(async (req, res) => {
+  const course = await Course.findByPk(req.params.id, {
+    include: [{model: User}],
+  });
+  try {
+  if (course) {
+    await course.update(req.body);
+    res.status(204).end();
+  }
+  } catch (error) {
+    console.log('ERROR: ', error.name);
+    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+      const errors = error.errors.map(err => err.message);
+      res.status(400).json({ errors });   
+    } else {
+      throw error;
+    }
+  }
+}));
+
+// Route that deletes an existing course.
+router.delete("/courses/:id", authenticateUser, asyncHandler(async (req, res) => {
+  const course = await Course.findByPk(req.params.id, {
+    include: [{model: User}],
+  });
+  if (course) {
+    await course.destroy();
+    res.status(204).end();
+  }
+}));
+
+module.exports = router;
