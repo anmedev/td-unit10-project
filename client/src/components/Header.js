b5b80a56cb66e12dc7e2209c11c13988c6317e28
Anelle Mensah <aam2ds@virginia.edu>
2023-12-07 17:11:31 -0500

    create Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+// import { useContext } from "react";
+// import UserContext from "../context/UserContext";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="wrap header--flex">
+        <Link to="/"><h1 className="header--logo">Courses</h1></Link>
+        <nav>
+          <>
+            <ul className="header--signedout">
+              <li><Link to="/signup">Sign up</Link></li>
+              <li><Link to="/signin">Sign in</Link></li>
+            </ul>
+          </>
+          
+          <>
+            {/* <span>Welcome {authUser.name}</span> */}
+            {/* <ul className="header--signedin">
+              <li><Link to="/signout">Sign out</Link></li>
+            </ul> */}
+          </>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+// const Header = () => {
+//   const { authUser } = useContext(UserContext);
+//   return (
+//     <header>
+//       <div className="wrap header--flex">
+//         <Link to="/">
+//           <h1 className="header--logo">Courses</h1>
+//         </Link>
+//         <nav>
+//           {authUser === null ? (
+//             <>
+//               <ul className="header--signedout">
+//                 <li>
+//                   <Link to="/signup">Sign up</Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/signin">Sign in</Link>
+//                 </li>
+//               </ul>
+//             </>
+//           ) : (
+//             <>
+//               <span>Welcome {authUser.name}</span>
+//               <ul className="header--signedin">
+//                 <li>
+//                   <Link to="/signout">Sign out</Link>
+//                 </li>
+//               </ul>
+//             </>
+//           )}
+//         </nav>
+//       </div>
+//     </header>
+//   );
+// };
+
+export default Header;
