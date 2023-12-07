756139b3761970d23bd5e49a428b14414fd372bc
Anelle Mensah <aam2ds@virginia.edu>
2023-12-07 13:31:33 -0500

    install axios and test connecting Rest API to React app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/courses")
+      .then(res => {
+        setCourses(res.data);
+        console.log(res);
+      })
+      .catch(error => {
+        console.log("Error fetching and parsing data", error);
+      })
+  }, []);
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          courses.map(course => <li key={course.id}>{course.title}</li>)
+        }
+      </ul>
     </div>
   );
+  
+  
+  
+  
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
