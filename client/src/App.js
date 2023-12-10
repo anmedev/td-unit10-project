3dd65225e1c23eeff22466f589afc256c29c68e7
Anelle Mensah <aam2ds@virginia.edu>
2023-12-10 18:23:58 -0500

    uncomment routes and import PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,30 @@ import './App.css';
 import './styles/reset.css';
 import './styles/global.css';
 import Header from './components/Header';
+import PrivateRoute from "./components/PrivateRoute";
 import Courses from './components/Courses';
 import CreateCourse from './components/CreateCourse';
 import CourseDetail from './components/CourseDetail';
-// import UpdateCourse from './components/UpdateCourse';
-// import UserSignIn from './components/UserSignIn';
-// import UserSignOut from './components/UserSignOut';
-// import UserSignUp from './components/UserSignUp';
+import UpdateCourse from './components/UpdateCourse';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
+import UserSignUp from './components/UserSignUp';
 
-//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-APP COMPONENT AND ROUTES-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-COMPONENT AND ROUTES-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 function App() {
   return (
     <div>
       <Header />
       <Routes>
         <Route path="/" element={<Courses />} />
-        <Route path="/courses/create" element={<CreateCourse/>} />
         <Route path="/courses/:id" element={<CourseDetail/>} />
-        {/* <Route path="/courses/:id/update" element={<UpdateCourse />}/> */}
-        {/* <Route path="/signin" element={<UserSignIn />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/courses/create" element={<CreateCourse/>} />
+          <Route path="/courses/:id/update" element={<UpdateCourse />}/>
+        </Route>
+        <Route path="/signin" element={<UserSignIn />} />
         <Route path="/signup" element={<UserSignUp />} />
-        <Route path="/signout" element={<UserSignOut  />} /> */}
+        <Route path="/signout" element={<UserSignOut  />} />
       </Routes>
     </div>
   );
