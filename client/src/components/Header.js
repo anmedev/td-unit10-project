c0545065b67beb57675bf9fe5a56381419da3962
Anelle Mensah <aam2ds@virginia.edu>
2023-12-10 18:25:34 -0500

    add authenticated user to nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,66 +1,35 @@
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-IMPORTS-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 import { Link } from "react-router-dom";
-// import { useContext } from "react";
-// import UserContext from "../context/UserContext";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 const Header = () => {
+  const { authUser } = useContext(UserContext);
+  
   return (
     <header>
       <div className="wrap header--flex">
         <h1 className="header--logo"><Link to="/">Courses</Link></h1>
         <nav>
+          {authUser === null?
           <>
             <ul className="header--signedout">
               <li><Link to="/signup">Sign up</Link></li>
               <li><Link to="/signin">Sign in</Link></li>
             </ul>
           </>
-          
+          :
           <>
-            {/* <span>Welcome {authUser.name}</span> */}
-            {/* <ul className="header--signedin">
+            <ul className="header--signedin">
+            <li>Welcome, {authUser.firstName} {authUser.lastName}!</li>
               <li><Link to="/signout">Sign out</Link></li>
-            </ul> */}
+            </ul>
           </>
+        }
         </nav>
       </div>
     </header>
   );
 };
-
-// const Header = () => {
-//   const { authUser } = useContext(UserContext);
-//   return (
-//     <header>
-//       <div className="wrap header--flex">
-//         <Link to="/">
-//           <h1 className="header--logo">Courses</h1>
-//         </Link>
-//         <nav>
-//           {authUser === null ? (
-//             <>
-//               <ul className="header--signedout">
-//                 <li>
-//                   <Link to="/signup">Sign up</Link>
-//                 </li>
-//                 <li>
-//                   <Link to="/signin">Sign in</Link>
-//                 </li>
-//               </ul>
-//             </>
-//           ) : (
-//             <>
-//               <span>Welcome {authUser.name}</span>
-//               <ul className="header--signedin">
-//                 <li>
-//                   <Link to="/signout">Sign out</Link>
-//                 </li>
-//               </ul>
-//             </>
-//           )}
-//         </nav>
-//       </div>
-//     </header>
-//   );
-// };
 
 export default Header;
