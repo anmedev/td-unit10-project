fb1f02902918b017513062d8259510135c249b04
Anelle Mensah <aam2ds@virginia.edu>
2023-12-10 18:16:45 -0500

    add components to configure private routes and display validation errors

---
+++ client/src/components/ErrorsDisplay.js
@@ -0,0 +1,19 @@
+const ErrorsDisplay = ({ errors }) => {
+    let errorsDisplay = null;
+  
+    if (errors.length) {
+      errorsDisplay = (
+        <div> 
+          <h3 className="validation--errors">Validation Errors</h3>
+          <div className="validation-errors">
+            <ul>
+              {errors.map((error, i) => <li key={i}>{error}</li>)}
+            </ul>
+          </div>
+        </div>
+      );
+    }
+    return errorsDisplay
+  }
+  
+  export default ErrorsDisplay;
