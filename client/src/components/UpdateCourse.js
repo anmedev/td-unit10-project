0f0dfe876d7425b9402d358ee1d6b47d590ef2eb
Anelle Mensah <aam2ds@virginia.edu>
2023-12-10 18:22:51 -0500

    add functionality for components

---
+++ client/src/components/UpdateCourse.js
@@ -0,0 +1,63 @@
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-IMPORTS-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+//-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-COMPONENT-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+const UpdateCourse = () => {
+
+  // State
+  const [course, setCourse] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  // Fetches a single course from the REST API based on the course's id.
+  useEffect(() => {
+    const fetchCourse = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/courses/${id}`);
+        setCourse(response.data);
+      } catch (error) {
+        console.log("Error fetching and parsing data", error);
+      }
+    };
+    fetchCourse();
+  }, [id]);
+
+  // Event handler for the "Cancel" button.
+  const handleCancel = (e) => {
+    e.preventDefault();
+    navigate(`/courses/${id}`);
+  };
+    
+  if (course) {
+    return (
+      <main>
+        <div className="wrap">
+          <h2>Update Course</h2>
+          <form>
+            <div className="main--flex">
+              <div>
+                <label htmlFor="courseTitle">Course Title</label>
+                <input id="courseTitle" name="courseTitle" type="text" defaultValue={course.title} />
+                <p>By Joe Smith</p>
+                <label htmlFor="courseDescription">Course Description</label>
+                <textarea id="courseDescription" name="courseDescription" defaultValue={course.description} />
+              </div>
+              <div>
+                <label htmlFor="estimatedTime">Estimated Time</label>
+                <input id="estimatedTime" name="estimatedTime" type="text" defaultValue={course.estimatedTime} />
+                <label htmlFor="materialsNeeded">Materials Needed</label>
+                <textarea id="materialsNeeded" name="materialsNeeded" defaultValue={course.materialsNeeded} />
+              </div>
+            </div>
+            <button className="button" type="submit">Update Course</button><button className="button button-secondary" onClick={handleCancel}>Cancel</button>
+          </form>
+        </div>
+      </main>
+    );
+  }
+};
+
+
+export default UpdateCourse;
